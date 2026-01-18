0a5532ec929c6403bb148c28fb232a00c7f9f1b3
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-01-18 23:58:48 +0530

    feat : add dashboard form and get the dependencies

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,5 +1,17 @@
-public class Starter {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard_form.fxml"))));
+        stage.show();
     }
 }
